b5e445c29779d75da9c14e8ca978cfb817bfae57
Leesa Ward <leesa@doubleedesign.com.au>
2026-05-31 16:13:20 +1000

    Fix: Keep spans in generated HTML

--- src/Miniblocks/MiniblockPlugin.php
+++ src/Miniblocks/MiniblockPlugin.php
@@ -14,6 +14,7 @@ abstract class MiniblockPlugin {
         add_filter('mce_external_plugins', [$this, 'register_plugin'], 1, 1);
         add_filter('mce_buttons_2', [$this, 'register_button'], 5);
         add_filter('the_content', [$this, 'filter_out_miniblock_attributes_when_rendering_html'], 20);
+	    add_filter('tiny_mce_before_init', [$this, 'keep_spans_in_generated_html']);
     }
 
     public function register_plugin(array $plugins): array {
@@ -55,4 +56,13 @@ abstract class MiniblockPlugin {
 
         return str_replace('<?xml encoding="UTF-8">', '', $output);
     }
+
+	public function keep_spans_in_generated_html($tinymce_settings) {
+		$existing = $settings['extended_valid_elements'] ?? '';
+		$settings['extended_valid_elements'] = $existing
+			? $existing . ',span,span[*]'
+			: 'span,span[*]';
+
+		return $settings;
+	}
 }
