64ab55d2ee9a9a5c05de61f9aedb73ea86879f3c
Leesa Ward <leesa@doubleedesign.com.au>
2026-05-31 16:17:29 +1000

    Styling tweaks/fixes

--- src/EditorUI.php
+++ src/EditorUI.php
@@ -6,6 +6,7 @@ class EditorUI {
 
     public function __construct() {
         add_action('admin_enqueue_scripts', [$this, 'enqueue_tinymce_ui_styles']);
+	    add_filter('tiny_mce_before_init', [$this, 'explicitly_set_tinymce_style'], 5, 1);
     }
 
     public function enqueue_tinymce_ui_styles(): void {
@@ -16,4 +17,16 @@ class EditorUI {
             PluginEntrypoint::get_version()
         );
     }
+
+	/**
+	 * Ensure all instances of TinyMCE use the same base styling
+	 * @param $settings
+	 * @return array
+	 */
+	public function explicitly_set_tinymce_style($settings): array {
+		$settings['skin'] = 'lightgray';
+		$settings['theme'] = 'modern';
+
+		return $settings;
+	}
 }
