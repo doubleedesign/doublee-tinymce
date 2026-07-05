8eb7881e15f50ae7f29639d7131c04c6fdd694da
Leesa Ward <leesa@doubleedesign.com.au>
2026-07-05 18:11:52 +1000

    Remove "Add form" button that Ninja Forms adds above the editor

--- src/EditorUI.php
+++ src/EditorUI.php
@@ -7,6 +7,7 @@ class EditorUI {
     public function __construct() {
         add_action('admin_enqueue_scripts', [$this, 'enqueue_tinymce_ui_styles']);
 	    add_filter('tiny_mce_before_init', [$this, 'explicitly_set_tinymce_style'], 5, 1);
+		add_filter('ninja_forms_hide_add_form_button', '__return_true');
     }
 
     public function enqueue_tinymce_ui_styles(): void {
